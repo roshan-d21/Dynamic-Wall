af7fca8bed756366d9e4c88f3deeaa1fdb1d61b3
Roshan Daivajna <roshan.daivajna@gmail.com>
2020-08-17 19:19:53 +0530

    Update install script

--- install.sh
+++ install.sh
@@ -3,6 +3,16 @@
 cp -r ./Wallpapers ~/Pictures
 cp .wallpaper.sh ~/
 chmod +x ~/.wallpaper.sh
-mkdir ~/.config ~/.config/autostart
-printf "[Desktop Entry]\nType=Application\nExec=/home/${USER}/.wallpaper.sh\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_IN]=Dynamic-Wall\nName=Dynamic-Wall\nComment[en_IN]=A program that dynamically changes the wallpaper based on the time of the day\nComment=A program that dynamically changes the wallpaper based on the time of the day\n" > ~/.config/autostart/.wallpaper.sh.desktop
+
+if [ ! -d "${HOME}/.config" ]
+then
+    mkdir ~/.config
+fi
+
+if [ ! -d "${HOME}/.config/autostart" ]
+then
+    mkdir ~/.config/autostart
+fi
+
+printf "[Desktop Entry]\nType=Application\nExec=${HOME}/.wallpaper.sh\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_IN]=Dynamic-Wall\nName=Dynamic-Wall\nComment[en_IN]=A program that dynamically changes the wallpaper based on the time of the day\nComment=A program that dynamically changes the wallpaper based on the time of the day\n" > ~/.config/autostart/.wallpaper.sh.desktop
 ~/.wallpaper.sh &
